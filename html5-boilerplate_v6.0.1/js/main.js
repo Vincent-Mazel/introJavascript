f1ef110e9f7620b1db39dd750c045a50b24e4b25
Vincent Mazel <vincent.mazel@etu.univ-amu.fr>
2018-02-09 10:47:58 +0100

    Add files via upload
    super

---
+++ html5-boilerplate_v6.0.1/js/main.js
@@ -0,0 +1,16 @@
+(function() {
+	"use strict";
+	$('.case-blanche').css({
+		'background-color': 'black',
+		'font-weight': 'bolder',
+		'border': 'solid 1px white',
+		'color': 'white'
+	})
+
+	$('case-noire').css({
+		'background-color': 'white',
+		'font-weight': 'bolder',
+		'border': 'solid 1px black',
+		'color': 'black'
+	})
+}) ();
